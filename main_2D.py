fa031e352a1eec5def21b78b7fcf7195a3a41513
TheFuser <blueminecraft1968@gmail.com>
2021-05-19 18:58:53 +0200

    Splitted function transform into 2 other functions

--- main_2D.py
+++ main_2D.py
@@ -1,6 +1,7 @@
 from utils import *
 from PIL import Image
 import numpy as np
+#import pygame as pg
 
 class Ascii2D:
 
@@ -23,8 +24,60 @@ class Ascii2D:
         return symbol
 
 
+    def pic_to_matrix(self,longueur:int,largeur:int,filename:str,ext:str):
+        image = Image.open(f'img/{filename}.{ext}')
+        image = image.convert('1')
+        image.save(f'img/{filename}_black.{ext}')
+        image = Image.open(f'img/{filename}_black.{ext}')
+        
+        long,larg = image.size
+        long,larg=list(np.linspace(0, long, longueur,dtype=int)),list(np.linspace(0, larg, largeur,dtype=int))
+        del long[-1]
+        del larg[-1]
 
-    def transform(self,longueur:int,largeur:int,filename:str,ext:str):
+        matrix = []
+        lst = []
+        for indexColor_largeur in larg:
+            for indexColor_longueur in long:
+                color = image.getpixel((int(indexColor_longueur),int(indexColor_largeur)))
+                lst.append(color)
+            matrix.append(lst)
+            lst=[]
+
+        return matrix
+
+    """
+    def pic_to_matrix2(self,longueur:int,largeur:int,filename:str,ext:str):
+        image = Image.open(f'img/{filename}.{ext}')
+        image = image.convert('1')
+        image.save(f'img/{filename}_black.{ext}')
+        image = Image.open(f'img/{filename}_black.{ext}')
+        
+
+
+        long,larg = image.size
+        print(long,larg)
+
+        image = image.resize((longueur,largeur), Image.ANTIALIAS)
+        image.save(f'img/{filename}_black.{ext}')
+
+        image = Image.open(f'img/{filename}_black.{ext}')
+        long,larg = image.size
+        
+        print(long,larg)
+        
+
+
+        I = np.asarray(image)
+        matrix = Image.fromarray(np.uint8(I))
+        
+        pixels = list(image.getdata())
+
+
+        return pixels
+    """
+
+    def transform(self,matrix:list):
         """Transform image (any extension) to ASCII Art
 
         Args:
@@ -37,35 +90,27 @@ class Ascii2D:
             None:
         """
 
-        image = Image.open(f'img/{filename}.{ext}')
-        image = image.convert('1')
-        image.save(f'img/{filename}_black.{ext}')
-        image = Image.open(f'img/{filename}_black.{ext}')
-        
-        long,larg = image.size
+        #assert (longueur<long and largeur<larg), "Invalid width or length"
+        longueur = len(matrix[0])
+        largeur = len(matrix)
 
-        assert (longueur<long and largeur<larg), "Invalid width or length"
-        
-        long,larg=list(np.linspace(0, long, longueur,dtype=int)),list(np.linspace(0, larg, largeur,dtype=int))
-
-        modified = [[0]*longueur for _ in range(largeur)]
+        #modified = [[0]*longueur for _ in range(largeur)]
     
-
-        del long[-1]
-        del larg[-1]
         
-        for indexModified_longueur,indexColor_longueur in zip(range(longueur),long):
-            for indexModified_largeur,indexColor_largeur in zip(range(largeur),larg):
-                color = image.getpixel((int(indexColor_longueur),int(indexColor_largeur)))
-                modified[indexModified_largeur][indexModified_longueur] = self.rvb(self,color)
+        for indexModified_longueur in range(longueur):
+            for indexModified_largeur in range(largeur):
+                matrix[indexModified_largeur][indexModified_longueur] = self.rvb(self,matrix[indexModified_largeur][indexModified_longueur])
                 
             
 
-        for i in modified:
+        for i in matrix:
             print("".join([str(k) for k in i]))
         
         return None
 
 
 
-Ascii2D.transform(Ascii2D,210,100,'test2','png')
+
+
+matrice = Ascii2D.pic_to_matrix(Ascii2D,210,100,'zemmour','jpg')
+Ascii2D.transform(Ascii2D,matrice)
