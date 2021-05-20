3c68623832e8abfa9cbbad0304b6aaf291d53319
TheFuser <blueminecraft1968@gmail.com>
2021-05-20 10:35:30 +0200

    Updated utils

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 class Constants:
+    import pygame.freetype as freetype
+    freetype.init()
     WIDTH = 1920
     HEIGHT = 1080
     LARGEUR_DIF = 15 #Check issue
     GRADIENTS = ['.', '-', '/', 'r', 'L', 'o', '*', "'", '_', '|', 'c', 'C', 'a', '&', '`', '+', '(', 'v', 'J', 'h', '%', ',', '<', ')', 'u', 'U', 'k', '$', '^', 'i', '1', 'n', 'Y', 'b', '#', ':', '?', ']', 'x', 'X', 'd', '@']
+    GAME_FONT = freetype.SysFont('Consolas', 12, bold=True)
 
 
 class Images:
