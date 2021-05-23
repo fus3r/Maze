f0d10e9642499b145a2102da321ed6414eb3a1ce
kramdur76 <31721504+kramdur76@users.noreply.github.com>
2021-05-23 12:19:57 +0200

    WAAAAAALS
    ptn ça marche, il faut maintenant augmenter la vitesse de calcul

--- Player.py
+++ Player.py
@@ -1,44 +1,105 @@
 import numpy as np
-from World import *
 from PIL import Image
-
 class Player:
-    def __init__(self, world)-> None:
-        self.world = world
+    def __init__(self)-> None:
         self.pos=np.array([0, 0])
-        self.dir=np.array([0, 0])
-        self.cam_size = 100, 50
+        self.dir=np.array([15, 6])
+        self.cam_size = 100, 40
         self.render_distance=10
         self.cam_focal_distance=.2
+        self.screen_scale=1
 
-    def generate_image(self):
+    def print_infos(self):
+        print('='*10+' PLAYER INFOS '+ '='*10)
+        print(f'Position {self.pos}')
+        print(f'dir {self.dir}')
+        print(f'CAMERA INFOS')
+        print()
+        
+        print(f'cam_size {self.cam_size}')
+        print(f'render_distance {self.render_distance}')
+        print(f'cam_focal_distance {self.cam_focal_distance}')
+        print(f'screen_scale {self.screen_scale}')
+    def rotate(self, theta):
+        if (0, 0) in self.dir:
+            return
+        x, y=self.dir
+        if x==0:
+            self.dir=np.array([np.sin(theta),np.cos(theta)])
+            
+        alpha=np.arctan(y/x)
+        self.dir=np.array([np.sin(alpha+theta),np.cos(alpha+theta)])
+
+    def set_dir_from_angle(self, theta):
+        self.dir= np.cos(theta),np. sin(theta)
+        self.dir=np.array(self.dir)
+    def generate_image(self, walls):
+        background_col=(0, 0, 0)
         w, h = self.cam_size
         render_image=[[0 for i in range(w)] for j in range(h)]#TODO : faire avec np
-        
+        assert (w, h)==(len(render_image[0]), len(render_image))
+        c=0
         for i in range(w):
             for j in range(h):
                 #on va faire du raytracing
-                for wall in self.world.walls:
-                    Ax, Ay, Bx, By = wall[0, 0], wall[0, 1], wall[1, 0], wall[1, 1]
-                    a, b, c = -(By-Ay), Bx-Ax, Ay*By-Ax*Ay
-                    self.dir/=((self.dir[0])**2+(self.dir[1])**2)**.5
-                    ux, uy= self.dir[0]-self.dir[1]*i*self.cam_size[0]/w, self.dir
-                    [1]+self.dir[0]*j*self.cam_size[1]/h
-                    aa, bb, cc = -(uy), ux, uy*self.pox[0]-self.pos[1]*ux
-                    p, q = aa*c-a*cc, a*bb-aa*b
-                    if q==0:
+                for wall in walls:
+                    #Init
+                    col=0
+                    a=np.array
+                    sq=lambda x:np.dot(x, x)
+                    orth=lambda s:a([-s[1], s[0]])
+                    norm=lambda x:((x[0])**2+(x[1])**2)**.5
+                    angle_between = lambda a, b : np.arccos(np.dot(a, b)/(norm(a)*norm(b)))
+
+                    #Computing
+                    A, B = wall[0], wall[1]                    
+                    s=self.cam_focal_distance
+                    d=a(self.dir)
+                    d_=orth(d)
+                    P=a(self.pos)
+                    d=d/norm(d)
+                    d_=d_/norm(d_)
+
+                    K=P+s*d+d_*(i-w/2)*self.screen_scale/w
+                    #print(f'A, P, K : {A, P, K }')
+                    assert sq(K)==sq(K)
+                    if sq(A-P)==0:
+                        render_image[j][i] = (255, 255, 255)
                         continue
-                    inter = p/q, (b*p/q+c)/-a #intersection des droites, pas des segments
-                    #à compléter
+                    papk, abpk=angle_between(A-P, K-P),angle_between(B-A, K-P)
+                    #print(f'papk, abpk: {papk, abpk}')
+                    if np.sin(abpk)==0:
+                        col=(0, 0, 0)
+                        render_image[j][i]=col
+                        continue
+                    assert papk==papk and abpk==abpk, (papk, abpk)
+                    AI = (B-A)*norm(P-A)*np.sin(papk)/np.sin(abpk)/norm(B-A)
+                    I = A+AI
+                    assert sq(AI)==sq(AI)
+                    if sq(A==P) == 2:
+                        print(f'A in P : {A} in {P}')
+                        c+=1
+                        render_image[j][i]=(255, 255, 255)
+                        continue
 
-                    dist=((self.pos[1]-inter[1])**2+(self.pos[0]-inter[0])**2)**.5
+                    #print(f'AI: {AI}')
+                    #print(f'I: {I}')
+                    dist=norm(I-P) #norm(I-K) ???
+                    if dist>self.render_distance:
+                        render_image[j][i] = background_col
+                        continue
+                    col = 255*(1-dist/self.render_distance)
+                    assert 0<=col<=255, f'col : {col,I, P,AI}'
+                    hm=2
+                    if abs(h/2-j)>= h*hm*s/self.screen_scale/dist:
+                        col=0
+                    
+                    if type(col)!=int: col=int(col)
 
-                    m=0
-                    col = 255+(m-b)*dist*255/self.render_distance
-                    col=int(col)
-                    render_image[i, j] = col
+                    render_image[j][i] = (col, col, col)
 
 
+        print(f"C {c}, {c/w/h}") 
     
         return render_image
 
--- World.py
+++ World.py
@@ -1,14 +1,33 @@
 import numpy as np
-
+from Player import Player
+import matplotlib.pyplot as plt
 
 class World:
     def __init__(self)->None:
         self.walls=[]
         self.walls.append(np.array([[4, 0], [-1, 10]]))
-        self.player=Player(self)
+        self.player=Player()
+        
         pass
 
-w=World()
+    def show_image(self):
+        image=self.player.generate_image(self.walls)
+        self.player.set_dir_from_angle(np.pi/2)
+        return image
 
-def test():
-    print("Hello World")
+
+
+w=World()
+a=np.array
+
+plt.show()
+while 1:
+    image=w.show_image()
+    image=a(image, dtype=np.uint8)
+    if plt.waitforbuttonpress():
+        print('Image type', type(image), type(image[0]), type(image[0][0]), type(image[0][0][0]))
+        plt.imshow(image)
+        w.player.pos+=a([1, 0], dtype=np.uint8)
+        w.player.print_infos()
+
+
