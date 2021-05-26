1e6e290a182530b9b382dfa9808f84ef0d764b1d
TheFuser <40115135+TheFuser@users.noreply.github.com>
2021-05-26 11:56:02 +0200

    Update Maze_generator.py

--- Maze_generator.py
+++ Maze_generator.py
@@ -1,9 +1,41 @@
 from main_2D import Ascii2D
+import random as r         
+from utils import *
 
-import random as r                  
 
-def generer_lab(x,y):
-    lab=[[r.choice([" ","|"]) for _ in range(x)] for _ in range(y)]
-    return lab
-for x in generer_lab(10,10):
-    print(*x)
+class MazeGen:
+
+    def in_maze(self,full,neighbors):
+        for i in neighbors:
+            try:
+                full[i]
+            except:
+                del i
+        return neighbors
+
+    def generate_maze(self,x,y):
+        lab=[[r.choice([" ","#"]) for _ in range(x)] for _ in range(y)]
+        return lab
+
+    def full_maze(self,x,y):
+        return [["#" for _ in range(x)] for _ in range(y)]
+
+    def maze(self,full,start_point:tuple):
+        x0,y0=start_point[0],start_point[1]
+        if x0>=len(full[0])-1 or y0>=len(full)-1:
+            return full
+        full[x0][y0] = " "
+        neighbors = [(i,j) for i in [x0-1,x0,x0+1] for j in [y0-1,y0,y0+1]]
+        neighbors = self.in_maze(self,full,neighbors)
+        return self.maze(self,full,r.choice(neighbors))
+
+
+
+
+if __name__=='__main__':
+    for x in MazeGen.generate_maze(MazeGen,10,10):
+        print(*x)
+    print("\n")
+    for i in MazeGen.maze(MazeGen,MazeGen.full_maze(MazeGen,20,20),(10,10)):
+        print(*i)
+    
