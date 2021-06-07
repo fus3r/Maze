30dd91895a3e4a0cea7ed13b300cc7111f985bed
TheFuser <40115135+TheFuser@users.noreply.github.com>
2021-06-07 14:59:09 +0200

    Almost Fixed Maze generator (remaining issue : infinite while)

--- Maze_generator.py
+++ Maze_generator.py
@@ -8,13 +8,14 @@ class MazeGen:
 
     def in_maze(self,full,neighbors):
         for i in neighbors:
-            try:
-                full[i]
-            except:
-                del i
-        for i in neighbors:
             if i[0]<0 or i[1]<0:
-                    del i
+                neighbors.remove(i)
+        
+        for i in neighbors:
+            try:
+                full[i[0]][i[1]]
+            except:
+                neighbors.remove(i)
         return neighbors
 
     def generate_maze(self,x,y):
@@ -26,24 +27,30 @@ class MazeGen:
 
     def maze(self,full,start_point:tuple,previous_neighbors:list):
         x0,y0=start_point[0],start_point[1]
-        if x0>=len(full[0])-1 or y0>=len(full)-1:
+        if x0==len(full[0])-1 and y0==len(full)-1:
             return full
+        
         full[x0][y0] = " "
         neighbors = MazeUtils.neighbors(x0,y0,diagonals=False)
         neighbors = self.in_maze(self,full,neighbors)
-        print(neighbors)
+        
         new_start_point = r.choice(neighbors)
+        print(f'Current neighbors : {neighbors}\nPrevious neighbors : {previous_neighbors}')
         if previous_neighbors is not None:
-            while new_start_point in previous_neighbors and MazeUtils.neighbors(new_start_point[0],new_start_point[1],diagonals=False).count(" ")>=2:
+            while new_start_point in previous_neighbors: #and MazeUtils.neighbors(new_start_point[0],new_start_point[1],diagonals=False).count(" ")>=2:
                 new_start_point = r.choice(neighbors)
-        print(new_start_point,MazeUtils.neighbors(new_start_point[0],new_start_point[1],diagonals=False).count(" "))
+            neighbors+=previous_neighbors
+        print(f'Choice made : {new_start_point}')
+        #print(new_start_point,MazeUtils.neighbors(new_start_point[0],new_start_point[1],diagonals=False).count(" "))
         """
         neighbors = MazeUtils.neighbors(x0,y0,diagonals=True)
         neighbors = self.in_maze(self,full,neighbors)
         """
         neighbors.append((x0,y0))
+        
         for i in full:  print(*i)
         print("\n")
+        
         return self.maze(self,full,new_start_point,neighbors)
 
 
@@ -55,6 +62,6 @@ if __name__=='__main__':
         print(*x)
     """
     print("\n")
-    for i in MazeGen.maze(MazeGen,MazeGen.full_maze(MazeGen,20,20),(10,10),None):
+    for i in MazeGen.maze(MazeGen,MazeGen.full_maze(MazeGen,20,20),(0,0),None):
         print(*i)
     
